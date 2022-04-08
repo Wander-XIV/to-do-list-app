dc2b1c4f91e714d532581ecc714cf69caabb8eee
Wander-XIV <aroy1420@gmail.com>
2022-04-08 17:14:47 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 function newItem() {
+  //1. Adding a new item to the list of items:
   let li = $("<li></li>");
   let inputValue = $("#input").val();
   li.append(inputValue);
@@ -9,6 +10,7 @@ function newItem() {
     $("#list").append(li);
   }
 
+  //2. Crossing out an item from the list of items:
   function crossOut() {
     li.toggleClass("strike");
   }
@@ -17,6 +19,7 @@ function newItem() {
     li.toggleClass("strike");
   });
 
+  //3(i). Adding the delete button "X":
   let crossOutButton = $("<crossOutButton></crossOutButton>");
   crossOutButton.append(document.createTextNode("X"));
   li.append(crossOutButton);
@@ -25,5 +28,6 @@ function newItem() {
   function deleteListItem() {
     li.addClass("delete");
   }
+  // 4. Reordering the items:
   $("#list").sortable();
 }
